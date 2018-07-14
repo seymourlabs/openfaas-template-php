82ef5e479f33fc63b146ba01346f073e1174569a
Andrew Seymour <commit@andrewseymour.co.uk>
2018-07-14 22:46:19 +0100

    Updated extensions

--- template/php7.0/function/php-extension.sh
+++ template/php7.0/function/php-extension.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
 echo "Installing PHP extensions"
+# Add your extensions in here
+#docker-php-ext-install mysqli pdo pdo_mysql
 
-apt-get update && apt-get install -y \
-        libfreetype6-dev \
-        libjpeg62-turbo-dev \
-        libmcrypt-dev \
-        libpng-dev \
-    && docker-php-ext-install -j$(nproc) iconv mcrypt \
-    && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
-    && docker-php-ext-install -j$(nproc) gd
+# If you needed phalcon, install this - WORK IN PROGRESS
+#echo "Installing Phalcon"
+#curl -s https://packagecloud.io/install/repositories/phalcon/stable/script.deb.sh | bash
--- template/php7.0/php-extension.sh
+++ template/php7.0/php-extension.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo "Skip installing PHP extensions for Core"
-exit 0
 
 apk add --no-cache \
     libmcrypt libmcrypt-dev && \
   NPROC=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || 1) && \
   docker-php-ext-install -j${NPROC} mcrypt && \
+  docker-php-ext-install mbstring && \
   apk del --no-cache \
     libmcrypt-dev
